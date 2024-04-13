f04088d6abcfb7a8cfd1727018fceac0568ab2b7
Nahidul Islam Siam <siamnahidul093@gmail.com>
2024-04-13 20:14:25 +0600

    added task

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,26 +1,65 @@
-import { createContext, useState } from "react";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { FacebookAuthProvider , GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+
+
 import auth from "../firebase/firebase.config";
 export const AuthContext = createContext(null)
 
 const AuthProvider = ({children}) => {
 const [user , setUser] = useState(null)
-    const registerUser = (email, password) =>{
+
+const googleProvider = new GoogleAuthProvider()
+const facebookProvider = new FacebookAuthProvider()      
+const registerUser = (email, password) =>{
 return createUserWithEmailAndPassword(auth,email,password)
 
     }
+
+ 
 
     const loginUser = (email, password) =>{
     return    signInWithEmailAndPassword(auth,email,password)
       
             }
   
+const googleLogin = () =>{
+    return signInWithPopup(auth,googleProvider)
+}
+
+const facebookLogin =()=>{
+    return signInWithPopup(auth,facebookProvider)
+}
+
+const logOut = ()=>{
+    return signOut(auth)
+}
+
     const authInfo ={
         registerUser,
         loginUser,
         user,
-        setUser
+        logOut,
+        setUser,
+        googleLogin
     }
+
+
+    useEffect(()=>{
+      const unsubscribe =  onAuthStateChanged(auth, (currentUser) => {
+            if (currentUser) {
+              // User is signed in, see docs for a list of available properties
+              // https://firebase.google.com/docs/reference/js/auth.user
+
+
+              console.log(currentUser);
+             
+            } else {
+              // User is signed out
+              // ...
+            }
+          });
+          
+    },[])
     return (
         <div>
           <AuthContext.Provider value={authInfo}>
